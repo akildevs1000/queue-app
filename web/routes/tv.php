988eb622678fb3350ecfcdd64c04a6ff0cd227e9
akildevs1000 <akildevs1000@gmail.com>
2025-06-24 16:20:32 +0400

    test

--- web/routes/tv.php
+++ web/routes/tv.php
@@ -18,8 +18,8 @@ Route::get("fetch_tv_settings",  function () {
             "https://picsum.photos/id/1043/800/600",
             "https://picsum.photos/id/1062/800/600"
         ],
-        "media_height" => "100%",
-        "media_width" => "100%",
+        "media_height" => "100px",
+        "media_width" => "100px",
         "ip" => "192.168.3.244",
         "port" => "7777",
     ]);
