c4f5a3b0e7126d3543b4017680a84b60e371e769
akildevs1000 <akildevs1000@gmail.com>
2025-06-24 16:42:20 +0400

    test

--- web/routes/tv.php
+++ web/routes/tv.php
@@ -10,16 +10,17 @@ Route::get("get-youtube-video-ids",  [YoutubeController::class, 'getYoutubeVideo
 Route::get("fetch_tv_settings",  function () {
 
     return response()->json([
-        "media_type" => "image",
+        "media_type" => "gif",
         "media_url" => [
-            "https://picsum.photos/id/1018/800/600",
-            "https://picsum.photos/id/1025/800/600",
-            "https://picsum.photos/id/1035/800/600",
-            "https://picsum.photos/id/1043/800/600",
-            "https://picsum.photos/id/1062/800/600"
+            "https://i.pinimg.com/originals/46/41/61/4641611401ecb508c625eebe448da663.gif",
+            // "https://picsum.photos/id/1018/800/600",
+            // "https://picsum.photos/id/1025/800/600",
+            // "https://picsum.photos/id/1035/800/600",
+            // "https://picsum.photos/id/1043/800/600",
+            // "https://picsum.photos/id/1062/800/600"
         ],
-        "media_height" => "600",
-        "media_width" => "700",
+        "media_height" => null,
+        "media_width" => null,
         "ip" => "192.168.3.244",
         "port" => "7777",
     ]);
