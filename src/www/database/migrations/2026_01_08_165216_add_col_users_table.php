3f3d6efe15934b7e0f7bfa8bcc58d0ea8b730714
akildevs1000 <akildevs1000@gmail.com>
2026-01-08 16:54:38 +0400

    removed extra file

--- src/www/database/migrations/2026_01_08_165216_add_col_users_table.php
+++ src/www/database/migrations/2026_01_08_165216_add_col_users_table.php
@@ -8,24 +8,20 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::table('companies', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->text('license_key')->nullable()->change();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::table('companies', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('license_key', 255)->nullable()->change();
         });
     }
